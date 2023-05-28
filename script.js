f6f52d6816602d6a953eb6cf571ab3f9d684b04c
Sakir Ali <118117387+SakirAli786@users.noreply.github.com>
2023-05-29 01:02:26 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+document.querySelector("#myButton").onclick = function(){
+    const h2 = document.querySelector("#myH1");
+    console.log(h2.style);
+    h2.style.color = "blue";
+    h1.style.color = "red";
+    h1.style.backgroundColor
+    =" green";
+}
